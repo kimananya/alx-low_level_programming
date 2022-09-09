0611232b198cdc5a381b8e5fae09c579e62173aa
kimananya <preshkimani@gmail.com>
2022-09-09 13:45:59 -0700

    Binary

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all single number of base 10 from 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
