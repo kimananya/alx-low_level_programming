94c32f23a7dcb2e45d47c25293243d763ee67f4d
kimananya <preshkimani@gmail.com>
2022-09-30 17:47:31 -0700

    Very difficult

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,24 +1,11 @@
 #include "main.h"
 
 /**
-* _isupper - checks for uppercase character
-* @letter: the chracter to be tracked
-* Return: 1 if c is uppercase, 0 otherwise
-*
-*/
-
-
-
-int _isupper(int letter)
-
-
-
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
 {
-
-
-
-return (letter >= 'A' && letter <= 'Z');
-
-
-
+	return (c >= 'A' && c <= 'Z');
 }
